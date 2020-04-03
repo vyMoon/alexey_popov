5d70bdab73c796bfd42d185ec56f84fdacbaefe2
Alex Popov <letmebetomorrow@mail.ru>
2020-04-03 19:03:49 +0300

    6 task ok

---
+++ 6-largets-smallest-functions.js
@@ -0,0 +1,21 @@
+// 6) Write largest and smallest functions that returns the largest and smallest number passed like a argument.
+
+// Example:
+// largest(2, 0.1, -5, 100, 3) // 100
+// smallest(2, 0.1, -5, 100, 3) // -5
+
+// 6 largest smallest functions
+
+function largest() {
+    return Array.from( arguments ).reduce( function (response, el) {
+        //choose bigger value
+        return response < el ? el : response;
+    });
+  };
+  
+  function smallest() {
+    return Array.from(  arguments ).reduce( function(response, el) {
+        //choose smaller value
+        return response > el ? el : response;
+    });
+};
