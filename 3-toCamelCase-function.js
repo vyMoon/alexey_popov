f6c085d5997aa67941c1c1c8119afa70f6d833d7
Alex Popov <letmebetomorrow@mail.ru>
2020-04-03 18:42:07 +0300

    3 task is ok

---
+++ 3-toCamelCase-function.js
@@ -0,0 +1,27 @@
+// 3) Complete the method/function so that it converts dash/underscore delimited words into camel casing. 
+// The first word within the output should be capitalized only if the original word was capitalized.
+
+// Example:
+// toCamelCase("the-stealth-warrior") // returns "theStealthWarrior"
+// toCamelCase("The_Stealth_Warrior") // returns "TheStealthWarrior"
+
+// 3 toCamelCase function
+
+function toCamelCase(str) {
+    // check dashes and underscore into the string
+    var dash = str.indexOf('-');
+    var underscore = str.indexOf('_');
+    // if there is no dashes or underscores into the string return string
+    if ( dash === -1 && underscore === -1 ) {
+      return str;
+    };
+    // chose the separator into the string 
+    // snd split the string into words
+    var separator = dash > -1 ? '-' : '_';
+    var wordsArr = str.split(separator);
+    // makes the response. 
+    // compound the respond string and makes the first letter of each words capitalized exept the first one 
+    return wordsArr.reduce( function(response, el, index) {
+      return response + el.charAt(0).toUpperCase() + el.slice(1)
+    });
+}
